0dfc7df8c65429ccc7408b649440aab6d5a073ea
Andrew Ferlitsch <Andrew Ferlitsch>
2017-01-23 18:14:19 -0800

    get data per category when opened

--- r.js
+++ r.js
@@ -18,18 +18,26 @@ technical.controller( 'rCtrl', function( $scope, $http, $location, $anchorScroll
 						  id: 3
 						}
 					  ];
-	$scope.random 	= pick3( $scope.questions );
+	$scope.random 	= $scope.questions;
 	$scope.show 	= false;
 	$scope.answers 	= false;
-	$http({
-        method : "GET",
-        url : "load.php",
-		params: { category: $scope.subject }
-    }).then(function mySucces(response) {
-        $scope.questions = response.data;
-		$scope.random 	 = pick3( $scope.questions );
-    }, function myError(response) {
-    });
+
+	$scope.scrollTo = function() {
+      $location.hash( $scope.name );
+      $anchorScroll();
+	  
+	  if ( $scope.show ) {
+		$http({
+			method : "GET",
+			url : "load.php",
+			params: { category: $scope.subject }
+		}).then(function mySucces(response) {
+			$scope.questions = response.data;
+			$scope.random 	 = pick3( $scope.questions );
+			}, function myError(response) {
+		});
+	  }
+    }
     
 	$scope.better = function( id ) {
 		showBetter( $scope.subject, id );
@@ -40,10 +48,7 @@ technical.controller( 'rCtrl', function( $scope, $http, $location, $anchorScroll
 	$scope.suggest = function() {
 		showSuggest( $scope.subject );
 	}
-	$scope.scrollTo = function() {
-      $location.hash( $scope.name );
-      $anchorScroll();
-    }
+
 	$scope.scrollTo = function() {
       $location.hash( $scope.name );
       $anchorScroll();
