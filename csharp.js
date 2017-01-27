def71f45be00a20e8526a169d8a27dba90961fa8
Andrew Ferlitsch <Andrew Ferlitsch>
2017-01-27 13:33:01 -0800

    rewrite inner UI views

--- csharp.js
+++ csharp.js
@@ -6,21 +6,11 @@ technical.controller( 'csharpCtrl', function( $scope, $http, $location, $anchorS
 						  answer: "",
 						  rank: 1,
 						  id: 1
-						},
-						{ question: "placeholder 2",
-						  answer: "",
-						  rank: 2,
-						  id: 2
-						},
-						{ question: "placeholder 3",
-						  answer: "",
-						  rank: 3,
-						  id: 3
 						}
 					  ];
 	$scope.random 	= $scope.questions;
 	$scope.show 	= false;
-	$scope.answers 	= false;
+	$scope.rank     = 1;
 
 	$scope.scrollTo = function() {
       $location.hash( $scope.name );
@@ -33,20 +23,22 @@ technical.controller( 'csharpCtrl', function( $scope, $http, $location, $anchorS
 			params: { category: $scope.subject }
 		}).then(function mySucces(response) {
 			$scope.questions = response.data;
-			$scope.random 	 = pick3( $scope.questions );
+			$scope.random 	 = pickNext( $scope.questions, 1 );
 			}, function myError(response) {
 		});
 	  }
     }
 	
-	$scope.better = function( id ) {
-		showBetter( $scope.subject, id );
+	$scope.Harder = function() {
+		if ( $scope.rank != 3 )
+			$scope.rank++;
+		$scope.random = pickNext( $scope.questions, $scope.rank );
 	}
-	$scope.rank = function( id ) {
-		showRank( $scope.subject, id );
-	}
-	$scope.suggest = function() {
-		showSuggest( $scope.subject );
+	
+	$scope.Easier = function () {
+		if ( $scope.rank != 1 )
+			$scope.rank--;
+		$scope.random = pickNext( $scope.questions, $scope.rank );
 	}
 
 	$scope.scored = false;
