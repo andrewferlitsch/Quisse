b3dda28a00ad6df75631ff1f46a7b02c79de59e6
Andrew Ferlitsch <Andrew Ferlitsch>
2017-01-31 19:41:15 -0800

    header improv

--- head.js
+++ head.js
@@ -20,10 +20,12 @@ technical.controller( 'interviewCtrl', function( $scope ) {
 		restrict: 'A',
 		template: "<div style='text-align: center'>" +
 				  "<h1>The Technical Interview</h1>" +
+				  
 				  "</div>" +
 				  "<ul class='w3-navbar w3-green'>" +
 				  "	<li><a href='preparing-for-technical-interview.html'>Interview Guide</a></li>" +
 				  "	<li><a href='coding-examples.html'>Coding Examples</a></li>" +
+				  "<span style='font-size: 9pt' class='w3-right'>Questions: <span class='w3-badge w3-tiny w3-teal'>510</span></span>" +
 				  "</ul>" +
 				  "<br/>" +
 				  "<label for='category' class='w3-label'>Select Category:</label>" +
