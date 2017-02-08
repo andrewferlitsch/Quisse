80a973356e3252873020a9c51e670f0bd2c66a17
Andrew Ferlitsch <Andrew Ferlitsch>
2017-02-08 07:47:30 -0800

    improving UX/responsiveness to Guide

--- head.js
+++ head.js
@@ -18,17 +18,7 @@ technical.controller( 'interviewCtrl', function( $scope ) {
 .directive( "interview", function() {
 	return {
 		restrict: 'A',
-		template: "<div style='text-align: center'>" +
-				  "<h1>The Technical Interview</h1>" +
-				  "</div>" +
-				  "<ul class='w3-navbar w3-green'>" +
-				  "	<li><a href='/'>Flashcards</a></li>" +
-				  "	<li><a href='preparing-for-technical-interview.html'>Interview Guide</a></li>" +
-				  "	<li><a href='coding-examples.html'>Coding Examples</a></li>" +
-				  "<span style='font-size: 9pt' class='w3-right'>Questions: <span class='w3-badge w3-tiny w3-teal'>604</span></span>" +
-				  "</ul>" +
-				  "<br/>" +
-				  "<label for='category' class='w3-label'>Select Skill Category:</label>" +
+		template: "<label for='category' class='w3-label'>Select Skill Category:</label>" +
 				  "<select name='category' id='category' class='w3-input' ng-model='category' ng-change='showCategory( category)'>" +
 				  "	<option ng-repeat='category in categories' value={{category}}>{{category}}</option>" +
 				  "</select>"
