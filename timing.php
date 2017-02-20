67efe547cd1766d17e41e7c7aeac880253933b7a
Andrew Ferlitsch <Andrew Ferlitsch>
2017-02-19 23:32:49 -0800

    JS code examples

--- timing.php
+++ timing.php
@@ -1,13 +1,15 @@
 <?php
-if( isset( $_POST['category'] ) ) {
-	$session  = $_POST['session'];
-	$category = $_POST['category'];
-	$id       = $_POST['id'];
-	$type     = $_POST['type'];
-	$time     = $_POST['time'];
-	
-	$txt = $session . "," . $category . "," . $id . "," . $type . "," . $time;
-	$file = file_put_contents( "timing.csv", $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
-	fclose( $file );
+$timing = $_POST[ 'timing' ];
+for ( $i = 0; $i < count( $timing ); $i++ ) {
+		$row = $timing[ $i ];
+		$session  = $row['session'];
+		$category = $row['category'];
+		$id       = $row['id'];
+		$type     = $row['type'];
+		$time     = $row['time'];
+		
+		$txt = $session . "," . $category . "," . $id . "," . $type . "," . $time;
+		$file = file_put_contents( "timing.csv", $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+		fclose( $file );
 }
 ?>
