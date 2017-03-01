5e8394354a47dbcbe7e51a998454251f80e2f2ce
Andrew Ferlitsch <Andrew Ferlitsch>
2017-03-01 12:06:31 -0800

    fine tune UI

--- nav.js
+++ nav.js
@@ -4,9 +4,8 @@ technical.controller( 'navCtrl', function( $scope, $rootScope ) {
 .directive( "nav", function() {
 	return {
 		restrict: 'A',
-		template: "<div style='text-align: center'>" +
+		template: "<div style='text-align: center; margin-top: -25px; margin-bottom: -10px'>" +
 				  "<img src='quisse.png'/>" +
-				  "<h4 style='margin-top: -15px'>The Technical Interview</h4>" +
 				  "</div>" +
 				  "<ul class='w3-navbar w3-green'>" +
 				  "	<li><a href='/'>Flashcard</a></li>" +
