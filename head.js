99742a6f68ec757de7944ba987bd110bc55b75f8
Andrew Ferlitsch <Andrew Ferlitsch>
2017-03-10 11:41:43 -0800

    remove dead code

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 technical.controller( 'interviewCtrl', function( $scope ) {
 	$scope.categories = [ "Agile", "Algorithms", "AngularJS", "C", "C++", "C#", "CSS", "Data Science", "DOS", 
 						  "HTML", "HTML5", "Java", "Javascript", "JQuery", "MySQL", "MySQL DBA", "Network", "OOP Design", 
-						   "OS", "QA", "R", "Python", "Security", "Web" ];
+						  "OS", "QA", "R", "Python", "Security", "Web" ];
 	$scope.view = [];
 	for ( var cat in $scope.categories ) {
 		$scope.view[ cat ] = false;
@@ -20,7 +20,7 @@ technical.controller( 'interviewCtrl', function( $scope ) {
 	return {
 		restrict: 'A',
 		template: "<h4 style='margin-top: -20px; text-align: center; color: steelblue'>The Technical Interview</h4>" +
-				  "<p>Use our 800 question/answer dataset to practice a technical phone screen.</p>" +
+				  "<p>Use our 900 question/answer dataset to practice a technical phone screen.</p>" +
 				  "<label for='category' class='w3-label'>Select Skill Category:</label>" +
 				  "<select name='category' id='category' class='w3-input' ng-model='category' ng-change='showCategory( category)'>" +
 				  "	<option ng-repeat='category in categories' value={{category}}>{{category}}</option>" +
