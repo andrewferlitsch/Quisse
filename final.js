92dbf8d84b962d46aabca601879fe2f627c6b855
Andrew Ferlitsch <Andrew Ferlitsch>
2017-03-22 14:00:16 -0700

    redo instruction link at bottom of page

--- final.js
+++ final.js
@@ -2,7 +2,6 @@
 technical.controller( 'finalCtrl', function( $scope, $rootScope, $location, $anchorScroll ) {
 	$scope.title 	= "Final Score";
 	$scope.show 	= true;
-	$scope.showleft = false;
 	$scope.totalQuestions = 0;
 	$scope.totalCorrect   = 0;
 	$scope.percentCorrect = "";
@@ -36,20 +35,8 @@ technical.controller( 'finalCtrl', function( $scope, $rootScope, $location, $anc
 	return {
 		restrict: 'A',
 		template: "<div class='w3-container'>" +
-				  "	<h3>Preparing for a Technical Phone Screen</h3>" +
-				  "	<p>Questions are selected at random and may increase or decrease in difficulty, depending on your answers and time spent.</p>" +
-				  "	<p style='color: orange; cursor: pointer' ng-click='showleft=!showleft'>&#9205; Instructions</p>" +
-				  "	<div ng-show='showleft'>" + 
-				  "		<ol>" +
-				  "			<li>Pick categories to be tested on.</li>" +
-				  "			<li>Click on the category to expand it, which will display randomly selected questions as flash cards at an initial level of difficulty.</li>" +
-				  "			<li>As you answer questions (in)correctly, the level of difficulty will progressively increase or decrease.</li>" +
-				  "			<li>Click on the flash card to display the answer.</li>" +
-				  "			<li>You can self-score by checking the Correct box.</li>" +
-				  "			<li>Your score will automatically accumulate, but you can reset your score in a category by selecting the reset button.</li>" +
-				  " 		<li>When completed, your results are displayed at the bottom of the page.</li>" +
-				  "		</ol>" +
-				  "	</div>" +
+				  "	<h3>Preparing for a Technical Interview</h3>" +
+				  "	<p style='color: orange; cursor: pointer' ng-click='showPage( \"About\" );'>&#9205; Instructions - See About</p>" +
 				  "</div>"
 	}
 });
