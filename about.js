56c6c05fb39a555edd84d2e4d13e9d26c5be4440
Andrew Ferlitsch <Andrew Ferlitsch>
2017-03-29 12:18:05 -0700

    add algorithm details to about

--- about.js
+++ about.js
@@ -1,6 +1,8 @@
 // Copyright(c), 2016-2017, Andrew Ferlitsch, All Rights Reserved
 technical.controller( 'aboutCtrl', function( $scope ) {
 	$scope.version = "1.2.2";
+	$scope.showflipdetails = false;
+	$scope.showquizdetails = false;
 })
 .directive( "about", function() {
 	return {
