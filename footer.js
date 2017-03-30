23640a53445b7c11547fe81ac959b669d63a5689
Andrew Ferlitsch <Andrew Ferlitsch>
2017-03-30 09:56:14 -0700

    fix redirect bug in footer.js

--- footer.js
+++ footer.js
@@ -4,10 +4,10 @@ technical.controller( 'footerCtrl', function( $scope ) {
 	return {
 		restrict: 'A',
 		template: "<div class='w3-container' style='color: orange; text-align: center;'>" +
-				  "		<a href='/' ng-click='showPage( \"Advice\" )'>Advice</a>" +
-				  "		<a href='/' ng-click='showPage( \"About\" );'>About</a>" +
-				  "		<a href='/' ng-click='showPage( \"Contact\" );'>Contact</a>" +
-				  "		<a href='/' ng-click='showPage( \"Contributor\" );'>Contributors</a>" +
+				  "		<a href='javascript:void(0)' ng-click='showPage( \"Advice\" )'>Advice</a>" +
+				  "		<a href='javascript:void(0)' ng-click='showPage( \"About\" );'>About</a>" +
+				  "		<a href='javascript:void(0)' ng-click='showPage( \"Contact\" );'>Contact</a>" +
+				  "		<a href='javascript:void(0)' ng-click='showPage( \"Contributor\" );'>Contributors</a>" +
 				  " </div>" +
 				  "<div style='text-align: center; color: gray'><br/>Copyright(c) 2016-17, Andrew Ferlitsch, Co-Organizer, Portland Data Science Group</div>"
 	}
