6397c7a95efae7b8809aa47fe3da7e6576af3eaf
Andrew Ferlitsch <Andrew Ferlitsch>
2017-04-17 21:08:33 -0700

    fix close() did not work

--- head.js
+++ head.js
@@ -64,7 +64,7 @@ technical.controller( 'interviewCtrl', function( $scope, $rootScope ) {
 				  //"	<option ng-repeat='category in categories' value={{category}}>{{category}}</option>" +
 				  //"</select>" 
 				  "<div class='w3-sidebar w3-bar-block w3-collapse w3-card-2' style='width:200px;' id='sidebar'>" +
-				  "	<button class='w3-bar-item w3-button w3-hide-large' onclick'w3_close()'>Close &times;</button>" +
+				  "	<button class='w3-bar-item w3-button w3-hide-large' onclick='w3_close()'>Close &times;</button>" +
 				  "	<a href='#' class='w3-bar-item w3-button' ng-repeat='category in categories' ng-click='showCategory( category)'>{{category}}</a>" +
 				  "</div>"
 	}
